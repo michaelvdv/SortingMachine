318b442066ecc97a2990ce2220097aab4cf8ea3e
Frankthedankengine <frank.049@hotmail.com>
2016-05-27 12:04:26 +0200

    Add files via upload

---
+++ main_thread.java
@@ -0,0 +1,37 @@
+
+public class main_thread {
+	
+	public enum State { 
+		INITIAL, START, PUSH, DETECT, // complete list of states the software goes through
+		SORT, RESET, PAUSE, ABORT 
+	}
+	
+	State state = State.INITIAL;
+	
+	boolean startPause;
+	boolean abort;
+	boolean 
+	
+	public void main(){
+		while (true) {
+			switch(state) {
+			case INITIAL: 
+				
+			case START:
+				
+			case PUSH:
+				
+			case DETECT:
+				
+			case SORT:
+			
+			case RESET:
+				
+			case PAUSE:
+				
+			case ABORT:
+
+			}
+		}	 
+	}
+}
